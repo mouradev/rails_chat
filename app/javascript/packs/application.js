772d9dee81b17144b2966d3b1018dad973ae3e72
Lucas Moura <contato@mouradev.com>
2021-02-15 04:20:22 +0000

    login and send messages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,7 @@ require("semantic-ui-sass")
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
