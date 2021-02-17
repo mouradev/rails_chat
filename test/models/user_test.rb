0bf69a44d4e5c586f32da80a24cb27007757a825
Lucas Moura <contato@mouradev.com>
2021-02-17 17:40:00 -0300

    user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid user" do
+    user = User.new(username: 'John', password: 'password')
+    assert user.valid?
+  end
+
+  test "invalid without username" do
+    user = User.new()
+    refute user.valid?
+  end
+
+  test "invalid with used username" do
+    user1 = User.new(username: 'John')
+    user2 = User.new(username: 'John')
+    refute user2.valid?
+  end
 end
