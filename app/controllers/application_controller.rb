547c3e5459ff52f1712de83bcbf13cf2729c8710
Lucas Moura <contato@mouradev.com>
2021-02-17 22:01:07 -0300

    improve ui

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:error] = "You must be logged in to perform that action"
       redirect_to login_path
     end
   end
