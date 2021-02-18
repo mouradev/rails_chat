0cd742f72c2d1d105b360d37f22c8166a12e8c98
Lucas Moura <contato@mouradev.com>
2021-02-18 00:04:02 -0300

    recover username in the form in case of login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,13 @@ class SessionsController < ApplicationController
   before_action :logged_in_redirect, only: [:new, :create]
 
   def new
+    @session = User.new
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
+    @session = User.new(username: params[:user][:username])
+    user = User.find_by(username: @session.username)
+    if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
